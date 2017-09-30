2b5c469f6cecab5ae11597959dfab2aa7ae4850e
Kristian <humanewolf@gmail.com>
2017-10-01 00:10:15 +0200

    Added admin clear stops command.

--- src/commands/admin.js
+++ src/commands/admin.js
@@ -4,6 +4,34 @@ module.exports = async app => {
         let user = msg.from;
         if (!app.config.ownerIDs.includes(user.id)) return;
 
-        //Else, do my admin shit.
+        let args = (msg.update.message.text).split(' ');
+
+        //Actions related to metro stops.
+        if (args[1] === 'stop') {
+            if (args[2] === 'clear') {
+                app.db.execute(
+                    'DELETE FROM stops',
+                    [],
+                    function (error) {
+                        if (error) {
+                            app.error.error('Failed to load from database (admin/stops/clear).');
+                            msg.reply('ERROR: Failed to load from database.');
+                            return;
+                        }
+
+                        msg.reply('Stopplisten er tømt.');
+                    }
+                );
+            }
+
+            if (args[2] === 'load') {
+
+            }
+        }
+
+        //Actions related to chats.
+        if (args[1] === 'chat') {
+
+        }
     });
 };
