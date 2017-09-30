8b3f96296b71d5d2bb3b54de8d0b4d232ae09a68
Kristian <humanewolf@gmail.com>
2017-09-30 22:11:52 +0200

    Formatting.

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,11 +1,11 @@
 module.exports = async app => {
     app.telegram.command(['/help', '/hjelp'], async msg => {
         msg.replyWithMarkdown(
-            '**Tilgjengelige Kommandoer**\n' +
-            '\t_/start_ - Registrer chatten som bruker av botten.\n' +
-            '\t_/oppdater_ - Aktiver eller deaktiver automatiske varsler om T-Banen.\n' +
-            '\t_/avganger <stedsnavn>_ - Vis de neste avgangene på en angitt stasjon.\n' +
-            '\t_/glemmeg_ - Slett informasjonen om deg/chatten fra databasen.'
+            '*Tilgjengelige Kommandoer*\n' +
+            '\t/start - _Registrer chatten som bruker av botten_.\n' +
+            '\t/oppdater - _Aktiver eller deaktiver automatiske varsler om T-Banen_.\n' +
+            '\t/avganger <stedsnavn> - _Vis de neste avgangene på en angitt stasjon_.\n' +
+            '\t/glemmeg - _Slett informasjonen om deg/chatten fra databasen_.'
         );
     });
 };
