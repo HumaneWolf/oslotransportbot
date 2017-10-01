61d5b31768f8e3dd6b1bf8a61388867c3a80a315
Kristian <humanewolf@gmail.com>
2017-10-01 13:15:14 +0200

    Now sends tweets to users.

---
+++ src/autoload/sendtelegram.js
@@ -0,0 +1,45 @@
+module.exports = app => {
+    setInterval(() => {
+        app.db.execute(
+            'SELECT * FROM chats WHERE tbane_updates=1',
+            [],
+            (error, results) => {
+                if (error) return app.error.error('Failed to load from database (sendtelegram/select/chats): ' + error);
+
+                let chats = results;
+
+                app.db.execute(
+                    'SELECT * FROM tweets WHERE notified_datetime IS NULL',
+                    [],
+                    (error, results) => {
+                        if (error)
+                            return app.error.error('Failed to load from database (sendtelegram/select/tweets): ' + error);
+
+                        results.forEach(async tweet => {
+                            let referral = 'twitter/tbanen';
+                            await chats.forEach(chat => {
+                                app.telegram.telegram.sendMessage(
+                                    chat.chat_id,
+                                    '*#TBANEN OPPDATERING:*\n' +
+                                    '\t ' + tweet.tweet_text + '\n' +
+                                    '_Fra: ' + referral + '_',
+                                    {
+                                        'parse_mode': 'Markdown'
+                                    }
+                                );
+                            });
+                            app.db.execute(
+                                'UPDATE tweets SET notified_datetime=CURRENT_TIMESTAMP WHERE tweet_id=?',
+                                [ tweet.tweet_id ],
+                                (error) => {
+                                    if (error)
+                                        return app.error.error('Failed to save to database (sendetelegram/update):' . error);
+                                }
+                            )
+                        });
+                    }
+                )
+            }
+        );
+    }, 60000);
+};
